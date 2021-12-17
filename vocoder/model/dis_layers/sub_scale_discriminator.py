67f21694980ff24b8becee7a4338d0ffe63f5f99
ainmukh <aynurmukh@gmail.com>
2021-12-18 00:24:53 +0300

    fix msd

--- vocoder/model/dis_layers/sub_scale_discriminator.py
+++ vocoder/model/dis_layers/sub_scale_discriminator.py
@@ -7,7 +7,7 @@ from torch.nn.utils import weight_norm
 class SubSDiscriminator(nn.Module):
     def __init__(self, pool: int = 0, kernel_size: int = 41, relu_slope: float = 1e-1, groups: int = 16):
         super(SubSDiscriminator, self).__init__()
-        self.pool = nn.AvgPool1d(4, 2, padding=2) if pool else nn.Identity()
+        self.pool = nn.AvgPool1d(4, 2, padding=2) if pool else None
         padding = (kernel_size - 1) // 2
         channels = 128
         stride = [2, 4, 4]
@@ -25,20 +25,23 @@ class SubSDiscriminator(nn.Module):
             channels = channels * 2
         layers += [
             weight_norm(nn.Conv1d(channels, channels, kernel_size, 1, groups=groups, padding=padding)),
-            weight_norm(nn.Conv1d(channels, channels, kernel_size=5, padding=2)),
-            weight_norm(nn.Conv1d(channels, 1, kernel_size=3, padding=1))
+            weight_norm(nn.Conv1d(channels, channels, kernel_size=5, padding=2))
         ]
+        self.post = weight_norm(nn.Conv1d(channels, 1, kernel_size=3, padding=1))
 
         self.layers = nn.ModuleList(layers)
         # exit(print(self.layers))
 
     def forward(self, x):
-        x = self.pool(x)
+        if self.pool is not None:
+            x = self.pool(x)
 
         features = []
-        for i, layer in enumerate(self.layers):
+        for layer in self.layers:
             x = layer(x)
-            if i + 1 != len(self.layers):
-                x = F.leaky_relu(x, 0.1)
+            x = F.leaky_relu(x, 0.1)
             features.append(x)
+        x = self.post(x)
+        features.append(x)
+
         return torch.flatten(x, 1, -1), features
