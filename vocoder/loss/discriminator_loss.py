e3138c27c30802741086112c69176507a166cbaf
ainmukh <aynurmukh@gmail.com>
2021-12-17 23:08:40 +0300

    fix device

--- vocoder/loss/discriminator_loss.py
+++ vocoder/loss/discriminator_loss.py
@@ -12,7 +12,7 @@ class DiscriminatorLoss(MSELoss):
         """
         loss = 0
         for gen, real in zip(doutput_gen, doutput_real):
-            loss_gen = super().forward(gen, torch.zeros(gen.size(), gen.device))
-            loss_real = super().forward(real, torch.ones(real.size(), real.device))
+            loss_gen = super().forward(gen, torch.zeros(gen.size(), device=gen.device))
+            loss_real = super().forward(real, torch.ones(real.size(), device=real.device))
             loss += loss_gen + loss_real
         return loss
