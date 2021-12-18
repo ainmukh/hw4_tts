19cfc821cb35e97ffcb660dd999784ac11ffffd3
ainmukh <aynurmukh@gmail.com>
2021-12-18 03:32:50 +0300

    new dis loss

--- vocoder/loss/discriminator_loss.py
+++ vocoder/loss/discriminator_loss.py
@@ -1,8 +1,9 @@
 import torch
+import torch.nn as nn
 from torch.nn import MSELoss
 
 
-class DiscriminatorLoss(MSELoss):
+class DiscriminatorLoss_(MSELoss):
     def forward(self, doutput_gen, doutput_real):
         """
         GAN discriminator D loss
@@ -16,5 +17,18 @@ class DiscriminatorLoss(MSELoss):
             loss_real = ((real - 1)**2).mean()
             # loss_gen = super().forward(gen, torch.zeros(gen.size(), device=gen.device))
             # loss_real = super().forward(real, torch.ones(real.size(), device=real.device))
+            loss += loss_gen + loss_real
+        return loss
+
+
+class DiscriminatorLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, doutput_gen, doutput_real):
+        loss = 0
+        for gen, real in zip(doutput_gen, doutput_real):
+            loss_gen = (gen ** 2).mean()
+            loss_real = ((real - 1) ** 2).mean()
             loss += loss_gen + loss_real
         return loss
